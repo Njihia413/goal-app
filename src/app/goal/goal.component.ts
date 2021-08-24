8ab0e79d86420e72052d8079fd2af7f3b72ca699
Maureen Njihia <maureennjihia468@gmail.com>
2021-08-24 14:30:22 +0300

    Add Http Client API and an  alert service

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert-service/alert.service';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { Quote } from '../quote-class/quote';
 
 @Component({
   selector: 'app-goal',
@@ -9,14 +13,15 @@ import { Goal } from '../goal';
 
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon', new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math', new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord', new Date(2030,3,14)),
-  ];
+
+  goals:Goal[];
+  alertService: AlertService;
+  quote!: Quote;
+
+constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient) {
+  this.goals = goalService.getGoals()
+  this.alertService = alertService;
+ }
 
 
   toggleDetails(index: any) {
@@ -29,6 +34,7 @@ export class GoalComponent implements OnInit {
 
       if(toDelete) {
         this.goals.splice(index,1);
+        this.alertService.alertMe("The goal has been deleted")
       }
     }
   }
@@ -40,9 +46,13 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal)
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    interface ApiResponse{
+      author:string;
+      quote:string;
+    }
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      this.quote = new Quote(data.author, data.quote)
+    })
   }
-
 }
